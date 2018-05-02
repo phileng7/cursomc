af494f9cdda33d9bfa1e3a33e694b68c118b91bb
Philip Mes <fmesy@outlook.com>
2018-05-01 22:21:22 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/cursomc/domain/Pedido.java
+++ src/main/java/com/cursomc/domain/Pedido.java
@@ -30,16 +30,13 @@ public class Pedido implements Serializable {
 	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
 	private Date instante;
 	
-	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
